2c16f23899dba6e1e50d7b47c3ecd0b02cf2526c
kokossVN <manh12345678951235789@gmail.com>
2024-03-07 19:19:56 +0700

    Update install.sh - generate configure file Update forcus_event.sh - read list app from config file

--- focus_event.sh
+++ focus_event.sh
@@ -4,10 +4,9 @@ ACTIVE_WINDOW=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | awk '{print $2}')
 WM_CLASS=$(xprop -id $ACTIVE_WINDOW WM_CLASS | awk -F '"' '{print $2}')
 WM_CLASS_TOLOWER=${WM_CLASS,,}
 
-# create log
-echo $WM_CLASS_TOLOWER >>/tmp/fcitx5-autosw-log.txt
-LIST_APP_EN=(kitty)
-LIST_APP_VI=(navigator)
+#check list form config file
+CONFIG_FILE=$XDG_CONFIG_HOME/fcitx5-auto-inputsw/config
+[ -f $CONFIG_FILE ] && . $CONFIG_FILE
 
 #to english
 for item in ${LIST_APP_EN[@]}; do
@@ -17,3 +16,5 @@ done
 for item in ${LIST_APP_VI[@]}; do
 	[ $WM_CLASS_TOLOWER = $item ] && fcitx5-remote -s unikey
 done
+# create log
+echo $WM_CLASS_TOLOWER >>/tmp/fcitx5-autosw-log.txt
--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
 EXTENSION_PATH=$HOME/.local/share/fcitx5/lua/imeapi/extensions/
+CONFIG_PATH=$XDG_CONFIG_HOME/fcitx5-auto-inputsw
 FILES=(auto-method.lua focus_event.sh)
-
+#
+echo "Generate symlink"
 for item in ${FILES[@]}; do
 	echo "ln -sf $PWD/$item $EXTENSION_PATH"
 	ln -sf $PWD/$item $EXTENSION_PATH
 done
-
-echo "done"
-
+#
+echo "Generate config file"
+echo "mkdir -p $CONFIG_PATH"
+mkdir -p $CONFIG_PATH
+# echo "echo -e \"LIST_APP_EN=()\nLIST_APP_VI=()\" >$CONFIG_PATH/config"
+if [ ! -f $CONFIG_PATH/config ]; then
+	echo "Initialize config"
+	echo -e "LIST_APP_EN=()\nLIST_APP_VI=()" >$CONFIG_PATH/config
+	echo "cat $CONFIG_PATH/config"
+	cat $CONFIG_PATH/config
+fi
+#
+echo "Done!"
 ls -la $EXTENSION_PATH
